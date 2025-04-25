3c3fe0652c71ea7436bf5b743c762fe9a9ff61cf
sanjolivm <147326363+sanjolivm@users.noreply.github.com>
2025-04-25 09:36:39 +0530

    Update p1.py

--- p1.py
+++ p1.py
@@ -1,6 +1,8 @@
-num = int(input('enter a number:)
-                if num%2 == 0:
-                print('even')
-                else:
-          print('odd')
+def fact(n):
+  if(n==0 or n==1):
+    return 1
+  else:
+    return (n)*fact(n-1)
+n=5
+print(fact(n))
                 
